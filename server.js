c34645ca38bdd176f8535c16e583987801265dcb
VPvpg <109874895+VPvpg@users.noreply.github.com>
2026-07-17 19:36:51 +0300

    Update server.js

--- server.js
+++ server.js
@@ -3,18 +3,17 @@ const { ExpressPeerServer } = require('peer');
 const app = express();
 const server = require('http').createServer(app);
 
-// Разрешаем CORS для всех
 app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
 
-// Настройка PeerServer: используем path: '/' для обработки вложенных запросов
 const peerServer = ExpressPeerServer(server, {
     path: '/'
 });
 
-app.use('/myapp', peerServer);
+// Слушаем дефолтный путь, который запрашивает клиент
+app.use('/peerjs', peerServer); 
 
 server.listen(process.env.PORT || 3000);
